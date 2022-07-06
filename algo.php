81e68cecdaaa8a6ac847be6b8f71ea95489f19d1
isaac <asxisal2@gmail.com>
2022-07-05 20:10:00 -0600

    first commit

---
+++ algo.php
@@ -0,0 +1,287 @@
+<meta charset="UTF-8">
+
+<title>Erisilerio</title>
+
+<link rel="shortcut icon" href="Logo.icon.png" type="image/x-icon">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Supermercado+One&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style2.css">
+
+
+
+<style>
+
+ 
+
+
+
+    *{
+
+        padding:0;
+
+        margin:0;
+
+        box-sizing: border-box;
+
+    }
+
+    
+
+    body{
+
+        background:#eee;
+
+        font-family:sans-serif;
+
+    }
+
+    
+
+    .banner-area{
+
+        width:100%;
+
+        height:100vh;
+
+        background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(back-img.jpg);
+
+        background-size:cover;
+
+    }        
+
+    
+
+    .content-area{
+
+        height:100%;
+
+        display:flex;
+
+        justify-content: center;
+
+        align-items: center;
+
+    }
+
+    
+
+   .content{
+
+        text-align: center;
+
+    }
+
+    
+
+    .content h1{
+
+        font-size:80px;
+
+        color:rgb(52, 103, 180);
+
+    }
+
+    
+
+    .content p{
+
+        font-size:30px;
+
+        color:rgb(0, 255, 255);
+
+    }
+
+    
+
+    .content .btn{
+
+        border:none;
+
+        outline:none;
+
+        padding:15px;
+
+        margin-top:100px;
+
+        border-radius: 4px;
+
+        color:rgb(255, 255, 255);
+
+        font-size: 18px;
+
+        cursor: pointer;    
+
+        background:rgb(20, 111, 185);             
+
+    }
+
+    
+
+    /* main area */
+
+    
+
+    .container {
+
+        width: 70%;
+
+        margin: 50px auto;
+
+        padding: 10px;
+
+}
+
+
+
+    .container .box {
+
+        width: 100%;
+
+        padding: 50px;
+
+        background: rgb(60, 66, 66);
+
+        margin-top: 50px;
+
+        border-radius: 10px;
+
+        box-shadow: 0 0 10px -3px rgba(0, 0, 0, .1);
+
+    }
+
+</style>
+
+</head>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Supermercado+One&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style2.css">
+
+<body>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style2.css">
+
+<div class="banner-area">
+
+    <div class="content-area">
+
+       <div class="content">
+
+          <h1>Erisilero</h1>
+
+          
+
+          <p><br>   Este canal fue  <br> creado para divertirnos</p>
+
+<br>
+
+          <a href="Minecraft.html" class="btn btn-successn">Minecraft</a>
+
+          <a href="quienes somos.html" class="btn btn-successn">Quines somos</a>
+
+          <a href="https://discord.gg/mQYuSRgJ3s" class="btn btn-successn">Discord</a>
+
+          <a href="Donde soy.html" class="btn btn-successn">De donde soy</a>
+
+          <a href="Amigoz.html" class="btn btn-successn">Amigos</a>
+
+          <a href="Gracias a.html" class="btn btn-successn">Gracias a </a>
+
+         <a href="visita mi canal.html" class="btn btn-successn">Visita mi canal  </a>
+
+<a href="<?php 
+
+session_start();
+session_destroy();
+
+header("Location: index.php");
+
+?>"></a>
+
+       </div>
+
+    </div>
+
+</div>
+
+
+
+
+
+<!--main area-->
+
+
+
+<div class="container">
+
+   <div class="box">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Supermercado+One&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style2.css">
+
+   
+
+    <link rel="stylesheet" href="css/estilo2.css"><button></button>
+
+    <link rel="stylesheet" href="css/estilo2.css">
+
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <body>
+
+     <link rel="stylesheet" href="css/style4.css">
+
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <h2>Erisilerio</h2>
+
+    <p> <br>Soy Erick <br>tengo 23 años<br> y hago<br> esto nomas<br> por diversión y <br>para pasar el<br> rato.</p>
+
+    <H2>Nuestras reglas de Twitch y Discord </H2>
+
+    <p><br>Spam y prácticas engañosas
+
+        <br>Contenido sensible 
+
+        <br> Contenido violento o peligroso
+
+        <br> información errónea</p>
+
+        <p>Pedir mod 
+
+        <h2>Esta regla se banea Permanentemente sin apelacion</h2></p>
+
+        <audio src="y2mate.com - Kaotica Eyeball Eiby La Sesión 47 Niño 30 x Samy the Wizart x Monster.mp3"></audio>
+
+
+
+<audio src="y2mate.com - Kaotica Eyeball Eiby La Sesión 47 Niño 30 x Samy the Wizart x Monster.mp3" autoplay="autoplay" loop="loop"></audio>
+
+
+
+         <h2><br>   El incumplimiento de estas reglas pueden terminar Con un ban o un mute  en twitch e igual en Discord </h2>
+
+    </h1>
+
+    <link rel="stylesheet" href="css/1.css">
+      </body>
